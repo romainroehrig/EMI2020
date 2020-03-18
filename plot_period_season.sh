7ad694016ab696105c932403596f28b16ae24792
SophieMbs <sophie.mbs@gmail.com>
2020-03-18 16:36:29 +0100

    Add script to execute plot_period and plot_season with correct arguments

---
+++ plot_period_season.sh
@@ -0,0 +1,49 @@
+./plot_period.py -f ../isccp/albisccp_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 1 
+./plot_period.py -f ../isccp/tauisccp_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 30 
+./plot_period.py -f ../isccp/cltisccp_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 100 
+./plot_period.py -f ../isccp/pctisccp_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 300 -M 900
+
+./plot_period.py -f ../modis/tautmodis_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 30
+./plot_period.py -f ../modis/cltmodis_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 100 
+./plot_period.py -f ../modis/cllmodis_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 100 
+./plot_period.py -f ../modis/clmmodis_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 100 
+./plot_period.py -f ../modis/clhmodis_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 100 
+./plot_period.py -f ../modis/pctmodis_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 300 -M 900 
+
+./plot_period.py -f ../calipso/cltcalipso_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 100 
+./plot_period.py -f ../calipso/cllcalipso_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 100 
+./plot_period.py -f ../calipso/clmcalipso_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 100 
+./plot_period.py -f ../calipso/clhcalipso_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 100
+./plot_period.py -f ../calipso/cltcalipsoice_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 100
+./plot_period.py -f ../calipso/cltcalipsoliq_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 100
+
+./plot_period.py -f ../modele/clt_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 100 
+./plot_period.py -f ../modele/cltl_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 100 
+./plot_period.py -f ../modele/cltm_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 100 
+./plot_period.py -f ../modele/clth_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 100
+
+
+
+./plot_season.py -f ../isccp/albisccp_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 1 
+./plot_season.py -f ../isccp/tauisccp_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 30 
+./plot_season.py -f ../isccp/cltisccp_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 100 
+./plot_season.py -f ../isccp/pctisccp_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 300 -M 900
+
+./plot_season.py -f ../modis/tautmodis_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 30
+./plot_season.py -f ../modis/cltmodis_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 100 
+./plot_season.py -f ../modis/cllmodis_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 100 
+./plot_season.py -f ../modis/clmmodis_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 100 
+./plot_season.py -f ../modis/clhmodis_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 100 
+./plot_season.py -f ../modis/pctmodis_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 300 -M 900 
+
+./plot_season.py -f ../calipso/cltcalipso_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 100 
+./plot_season.py -f ../calipso/cllcalipso_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 100 
+./plot_season.py -f ../calipso/clmcalipso_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 100 
+./plot_season.py -f ../calipso/clhcalipso_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 100
+./plot_season.py -f ../calipso/cltcalipsoice_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 100
+./plot_season.py -f ../calipso/cltcalipsoliq_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 100
+
+./plot_season.py -f ../modele/clt_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 100 
+./plot_season.py -f ../modele/cltl_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 100 
+./plot_season.py -f ../modele/cltm_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 100 
+./plot_season.py -f ../modele/clth_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -m 0 -M 100

dea80dc60ec31fe6dd7eabf2bf5845b4836a4d2b
SophieMbs <sophie.mbs@gmail.com>
2020-03-18 09:19:17 +0100

    Add script to execute plot_pct_tau_box with correct parameters

---
+++ plot_pct_tau_box.sh
@@ -0,0 +1,14 @@
+./plot_pct_tau_box.py -f ../isccp/clisccp_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -lt 45 -Lt 55 -lg -30 -Lg -21 -m 0 -M 8 -rg atlantic_ocean -s ISCCP
+./plot_pct_tau_box.py -f ../isccp/clisccp_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -lt 50 -Lt 55 -lg 20 -Lg 30 -m 0 -M 5 -rg eastern_europe -s ISCCP
+./plot_pct_tau_box.py -f ../isccp/clisccp_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -lt 33 -Lt 37 -lg 15 -Lg 22 -m 0 -M 3 -rg eastern_mediterranean -s ISCCP
+./plot_pct_tau_box.py -f ../isccp/clisccp_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -lt 19 -Lt 25 -lg 24 -Lg 30 -m 0 -M 3 -rg eastern_sahara -s ISCCP
+./plot_pct_tau_box.py -f ../isccp/clisccp_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -lt 37 -Lt 42.5 -lg 3 -Lg 8.5 -m 0 -M 3.5 -rg western_mediterranean -s ISCCP
+./plot_pct_tau_box.py -f ../isccp/clisccp_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -lt 20 -Lt 27 -lg -6 -Lg 4 -m 0 -M 8 -rg western_sahara -s ISCCP
+
+
+./plot_pct_tau_box.py -f ../modis/clmodis_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -lt 45 -Lt 55 -lg -30 -Lg -21 -m 0 -M 8 -rg atlantic_ocean -s Modis
+./plot_pct_tau_box.py -f ../modis/clmodis_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -lt 50 -Lt 55 -lg 20 -Lg 30 -m 0 -M 5 -rg eastern_europe -s Modis
+./plot_pct_tau_box.py -f ../modis/clmodis_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -lt 33 -Lt 37 -lg 15 -Lg 22 -m 0 -M 3 -rg eastern_mediterranean -s Modis
+./plot_pct_tau_box.py -f ../modis/clmodis_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -lt 19 -Lt 25 -lg 24 -Lg 30 -m 0 -M 3 -rg eastern_sahara -s Modis
+./plot_pct_tau_box.py -f ../modis/clmodis_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -lt 37 -Lt 42.5 -lg 3 -Lg 8.5 -m 0 -M 3.5 -rg western_mediterranean -s Modis
+./plot_pct_tau_box.py -f ../modis/clmodis_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -lt 20 -Lt 27 -lg -6 -Lg 4 -m 0 -M 8 -rg western_sahara -s Modis

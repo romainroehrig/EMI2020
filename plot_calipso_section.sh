cb7650726848bdeb3787eb504964588a07cfc448
SophieMbs <sophie.mbs@gmail.com>
2020-03-18 14:46:33 +0100

    Add script to execute plot for calipso sections (meridian and zonal) with correct arguments

---
+++ plot_calipso_section.sh
@@ -0,0 +1,27 @@
+./plot_calipso_meridian_section.py -f ../calipso/clcalipsoice_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -ilon 10 -m 0 -M 100 -rg atlantic_ocean 
+./plot_calipso_meridian_section.py -f ../calipso/clcalipsoice_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -ilon 58 -m 0 -M 100 -rg western_europe_and_sahara
+./plot_calipso_meridian_section.py -f ../calipso/clcalipsoice_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -ilon 110 -m 0 -M 100 -rg eastern_europe_and_sahara
+
+./plot_calipso_zonal_section.py -f ../calipso/clcalipsoice_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -ilat 85 -m 0 -M 100 -rg northen_europe
+./plot_calipso_zonal_section.py -f ../calipso/clcalipsoice_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -ilat 50 -m 0 -M 100 -rg mediterranean
+./plot_calipso_zonal_section.py -f ../calipso/clcalipsoice_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -ilat 15 -m 0 -M 100 -rg sahara
+
+
+
+./plot_calipso_meridian_section.py -f ../calipso/clcalipsoliq_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -ilon 10 -m 0 -M 100 -rg atlantic_ocean
+./plot_calipso_meridian_section.py -f ../calipso/clcalipsoliq_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -ilon 58 -m 0 -M 100 -rg western_europe_and_sahara
+./plot_calipso_meridian_section.py -f ../calipso/clcalipsoliq_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -ilon 110 -m 0 -M 100 -rg eastern_europe_and_sahara
+
+./plot_calipso_zonal_section.py -f ../calipso/clcalipsoliq_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -ilat 85 -m 0 -M 100 -rg northen_europe
+./plot_calipso_zonal_section.py -f ../calipso/clcalipsoliq_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -ilat 50 -m 0 -M 100 -rg mediterranean
+./plot_calipso_zonal_section.py -f ../calipso/clcalipsoliq_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -ilat 15 -m 0 -M 100 -rg sahara
+
+
+
+./plot_calipso_meridian_section.py -f ../calipso/clcalipso_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -ilon 10 -m 0 -M 100 -rg atlantic_ocean
+./plot_calipso_meridian_section.py -f ../calipso/clcalipso_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -ilon 58 -m 0 -M 100 -rg western_europe_and_sahara
+./plot_calipso_meridian_section.py -f ../calipso/clcalipso_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -ilon 110 -ilon 110 -m 0 -M 100 -rg eastern_europe_and_sahara
+
+./plot_calipso_zonal_section.py -f ../calipso/clcalipso_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -ilat 85 -m 0 -M 100 -rg northen_europe
+./plot_calipso_zonal_section.py -f ../calipso/clcalipso_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -ilat 50 -m 0 -M 100 -rg mediterranean
+./plot_calipso_zonal_section.py -f ../calipso/clcalipso_MED-44_ECMWF-ERAINT_evaluation_r1i1p1f2_CNRM-ALADIN64_v1_mon_200701-201612.nc -ilat 15 -m 0 -M 100 -rg sahara
